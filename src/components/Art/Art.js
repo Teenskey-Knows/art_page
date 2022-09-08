1893b137c3b3293558e8d993f02c0d661b90e963
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-09-08 08:20:01 +0300

    completed the basic skeleton

--- src/components/Art/Art.js
+++ src/components/Art/Art.js
@@ -1,9 +1,65 @@
 import React from 'react'
 import "../Art/Art.css"
+import {useState,useEffect} from 'react-router-dom'
+
+
 function Art() {
   return (
     <div className="body">
-        
+      <br />
+        <div className='second-body'>
+
+
+          <div className='art-card'>
+            <img className="art-image" src="https://pbs.twimg.com/media/Fb_HW3QXkAI-JVv?format=jpg&name=360x360" alt="" />
+            <div className="art-get-request-encloser">
+            <h3>Artist Name: Martoh Incognitoh</h3>
+            <h3>Artist Id: 1</h3>
+            <h3>Art Description:</h3>
+            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio laborum nobis consequatur dolores quaerat architecto itaque fuga ratione officiis sunt.</h2>
+            </div>
+          </div>
+
+
+
+          <div className='art-card'>
+            <img className="art-image" src="https://pbs.twimg.com/media/Fb_HW3QXkAI-JVv?format=jpg&name=360x360" alt="" />
+            <div className="art-get-request-encloser">
+            <h3>Artist Name: Martoh Incognitoh</h3>
+            <h3>Artist Id: 1</h3>
+            <h3>Art Description:</h3>
+            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio laborum nobis consequatur dolores quaerat architecto itaque fuga ratione officiis sunt.</h2>
+            </div>
+          </div>
+
+
+          <div className='art-card'>
+            <img className="art-image" src="https://pbs.twimg.com/media/Fb_HW3QXkAI-JVv?format=jpg&name=360x360" alt="" />
+            <div className="art-get-request-encloser">
+            <h3>Artist Name: Martoh Incognitoh</h3>
+            <h3>Artist Id: 1</h3>
+            <h3>Art Description:</h3>
+            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio laborum nobis consequatur dolores quaerat architecto itaque fuga ratione officiis sunt.</h2>
+            </div>
+          </div>
+
+
+
+          <div className='art-card'>
+            <img className="art-image" src="https://pbs.twimg.com/media/Fb_HW3QXkAI-JVv?format=jpg&name=360x360" alt="" />
+            <div className="art-get-request-encloser">
+            <h3>Artist Name: Martoh Incognitoh</h3>
+            <h3>Artist Id: 1</h3>
+            <h3>Art Description:</h3>
+            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio laborum nobis consequatur dolores quaerat architecto itaque fuga ratione officiis sunt.</h2>
+            </div>
+          </div>
+
+          <div></div>
+          <div></div>
+
+
+        </div>
 
     </div>
   )
