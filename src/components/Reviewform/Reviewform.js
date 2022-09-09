fb6bc58e84509f139c6e2844e576d8fcc1f5f744
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-09-09 16:54:29 +0300

    Form already set up

--- src/components/Reviewform/Reviewform.js
+++ src/components/Reviewform/Reviewform.js
@@ -3,9 +3,34 @@ import "../Reviewform/Reviewform.css"
 
 function Reviewform() {
   return (
-    <div>
+    <>
+    <div className='form'>
         <h2>Review Form</h2>
     </div>
+    <div className='structureform'>
+    <form action="">
+        <label htmlFor="User Id">User Id</label>
+        <br />
+        <input type="number" />
+        <br />
+        <label htmlFor="Art Id">Art Id To Review</label>
+        <br />
+        <input type="number" />
+        <br />
+        <label htmlFor="Rate Art">Rate Art</label>
+        <br />
+        <input type="number" />
+        <br />
+        <label htmlFor="Comment">Comment</label>
+        <br />
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <br />
+        <button type='submit'>Submit Review</button>
+
+    </form>
+    </div>
+    </>
+
   )
 }
 
